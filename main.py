4b9f0ae7fe12edd614d1369ec4010b137ad6a6a6
codetangle <trevor@devreason.com>
2025-08-08 21:11:38 -0500

    Add player rotation controls

--- main.py
+++ main.py
@@ -23,11 +23,13 @@ def main():
                 return
 
         screen.fill((0,0,0))
+        player.update(dt)
         player.draw(screen)
 
+        dt = clock.tick(60) / 1000
+        
         #call this last
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
