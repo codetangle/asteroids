2ec0c804b240fd8efe87bfaf9097c09289b20c5d
codetangle <trevor@devreason.com>
2025-08-08 21:27:36 -0500

    Add sprite groups

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
@@ -23,8 +27,11 @@ def main():
                 return
 
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+
+        updatable.update(dt)
+        
+        for item in drawable:
+            item.draw(screen)
 
         dt = clock.tick(60) / 1000
         
